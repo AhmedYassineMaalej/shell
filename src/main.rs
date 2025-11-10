4c4480f3240e05e2b47b2be4a2d8638ba8f07054
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2025-11-10 01:13:41 +0100

    support invalid commands

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
+    let stdin = io::stdin();
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    stdin.read_line(&mut buf).unwrap();
+
+    match buf.trim() {
+        s => println!("{s}: command not found"),
+    }
 }
